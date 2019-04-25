43b497f19d5f8b79ca723f2f425824ff8ae3aa2a
RumenKeranchev <devtestmail69@gmail.com>
2019-04-25 21:00:36 +0300

    initial commit

---
+++ ConsoleApplications/MainApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplications
+{
+	class MainApp
+	{
+		static void Main( string[] args )
+		{
+			var result = Task01_Solution.Solution( 5, new[] { 1, 2, 3 } );
+
+			Console.WriteLine( string.Join( ", ", result ) );
+		}
+	}
+}
---
+++ ConsoleApplications/Task01_Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApplications
+{
+	public static class Task01_Solution
+	{
+		public static int[] Solution( int n, int[] a )
+		{
+			var result = new int[ n ];
+
+			return result;
+		}
+	}
+}
